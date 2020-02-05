f2de829048002f1c5e63c38954f8b61398b91f4e
kajhemmingsen <kaj.hemmingsen.85@gmail.com>
2020-02-05 15:55:16 -0500

    added Scanner and File inputs to Workbench

--- main.go
+++ main.go
@@ -12,20 +12,31 @@ import (
 
 func init() {
 	fmt.Println("beginning simulation")
-	file1, _ := os.Open("data/servinsp1.dat")
-	file2, _ := os.Open("data/servinsp22.dat")
-	file3, _ := os.Open("data/servinsp23.dat")
-	component1 := component.NewComponent("Component 1", 1, file1)
-	component2 := component.NewComponent("Component 2", 2, file2)
-	component3 := component.NewComponent("Component 3", 3, file3)
+
+	//instantiating files to be read
+	//component files
+	componentFile1, _ := os.Open("data/servinsp1.dat")
+	componentFile2, _ := os.Open("data/servinsp22.dat")
+	componentFile3, _ := os.Open("data/servinsp23.dat")
+	//workbench files
+	benchFile1, _ := os.Open("data/ws1.dat")
+	benchFile2, _ := os.Open("data/ws2.dat")
+	benchFile3, _ := os.Open("data/ws3.dat")
+
+	//initializing objects
+	component1 := component.NewComponent("Component 1", 1, componentFile1)
+	component2 := component.NewComponent("Component 2", 2, componentFile2)
+	component3 := component.NewComponent("Component 3", 3, componentFile3)
 	p1 := product.NewProduct("product 1", []*component.Component{component1})
 	p2 := product.NewProduct("product 2", []*component.Component{component1, component2})
 	p3 := product.NewProduct("product 3", []*component.Component{component1, component3})
-	w1 := workbench.NewWorkbench("workbench 1", p1)
-	w2 := workbench.NewWorkbench("workbench 2", p2)
-	w3 := workbench.NewWorkbench("workbench 3", p3)
+	w1 := workbench.NewWorkbench("workbench 1", p1, benchFile1)
+	w2 := workbench.NewWorkbench("workbench 2", p2, benchFile2)
+	w3 := workbench.NewWorkbench("workbench 3", p3, benchFile3)
 	i1 := inspector.NewInspector("inspector 1", []*component.Component{component1}, []*workbench.Workbench{w1, w2, w3})
 	i2 := inspector.NewInspector("inspector 2", []*component.Component{component2, component3}, []*workbench.Workbench{w2, w3})
+
+	//starting threads
 	go i1.ReadData()
 	go i2.ReadData()
 }
--- pkg/workbench/workbench.go
+++ pkg/workbench/workbench.go
@@ -1,7 +1,9 @@
 package workbench
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/SYSC4005-Project/pkg/component"
 	"github.com/SYSC4005-Project/pkg/product"
@@ -11,6 +13,8 @@ type Workbench struct {
 	Name           string
 	Product        *product.Product
 	ComponentArray map[string][]*component.Component
+	File           *os.File
+	Scanner        *bufio.Scanner
 }
 
 func initComponentArray(product *product.Product) map[string][]*component.Component {
@@ -21,11 +25,12 @@ func initComponentArray(product *product.Product) map[string][]*component.Compon
 	return ComponentArray
 }
 
-func NewWorkbench(name string, product *product.Product) *Workbench {
+func NewWorkbench(name string, product *product.Product, file *os.File) *Workbench {
 	return &Workbench{
 		Name:           name,
 		Product:        product,
 		ComponentArray: initComponentArray(product),
+		File:           file,
 	}
 }
 
@@ -59,4 +64,8 @@ func (bench *Workbench) MakeProduct() {
 			fmt.Printf("Made %s", bench.Product.Name)
 		}
 	}
+}
+
+func (bench *Workbench) addScanner(scanner *bufio.Scanner) {
+	bench.Scanner = scanner
 }
