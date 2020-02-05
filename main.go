1a51ee315f11a29eb5d726e58bb90375dbaad5a4
kajhemmingsen <kaj.hemmingsen.85@gmail.com>
2020-02-05 14:53:39 -0500

    instatiated products and workbenches

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/SYSC4005-Project/pkg/component"
 	"github.com/SYSC4005-Project/pkg/inspector"
+	"github.com/SYSC4005-Project/pkg/product"
+	"github.com/SYSC4005-Project/pkg/workbench"
 )
 
 func init() {
@@ -16,9 +18,15 @@ func init() {
 	component1 := component.NewComponent("Component 1", 1, file1)
 	component2 := component.NewComponent("Component 2", 2, file2)
 	component3 := component.NewComponent("Component 3", 3, file3)
-	i := inspector.NewInspector("inspector 1", []*component.Component{component1})
+	i1 := inspector.NewInspector("inspector 1", []*component.Component{component1})
 	i2 := inspector.NewInspector("inspector 2", []*component.Component{component2, component3})
-	go i.ReadData()
+	p1 := product.NewProduct("product 1", []*component.Component{component1})
+	p2 := product.NewProduct("product 2", []*component.Component{component1, component2})
+	p3 := product.NewProduct("product 3", []*component.Component{component1, component3})
+	w1 := workbench.NewWorkbench("workbench 1", []*inspector.Inspector{i1}, p1)
+	w2 := workbench.NewWorkbench("workbench 2", []*inspector.Inspector{i1, i2}, p2)
+	w3 := workbench.NewWorkbench("workbench 3", []*inspector.Inspector{i1, i2}, p3)
+	go i1.ReadData()
 	go i2.ReadData()
 }
 func main() {
